9e770aa60c7139da4ab68e2fba9c687bcc7307cc
Entwickler <entwickler@davids-air.fritz.box>
2016-11-12 11:34:09 +0100

    Initial Commit

---
+++ Schul Manager/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schul Manager
+//
+//  Created by Entwickler on 12.11.16.
+//  Copyright © 2016 David Wiesner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Schul ManagerTests/Schul_ManagerTests.swift
@@ -0,0 +1,36 @@
+//
+//  Schul_ManagerTests.swift
+//  Schul ManagerTests
+//
+//  Created by Entwickler on 12.11.16.
+//  Copyright © 2016 David Wiesner. All rights reserved.
+//
+
+import XCTest
+@testable import Schul_Manager
+
+class Schul_ManagerTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
